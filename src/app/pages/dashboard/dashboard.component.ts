2cbaf9c0e1c86492a25df8a10122a2f1d8877365
Federico Marra <marrafederico@proton.me>
2026-05-31 18:39:37 +0200

    feat: add dashboard component and create activity features files

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
 
+//Material component
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MatCardModule, MatButtonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
